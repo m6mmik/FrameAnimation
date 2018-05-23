ec55da20cf32e8a2ee688be3a3a07d3c8b694d12
Unknown <andreaskuuskaru99@hotmail.com>
2018-05-23 15:32:20 +0300

    wdaw
    awd

---
+++ FrameAnimation/obj/Debug/android/src/md504e2fdbc06f17b311f3b8e4eeed307fd/Activity1.java
@@ -0,0 +1,56 @@
+package md504e2fdbc06f17b311f3b8e4eeed307fd;
+
+
+public class Activity1
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onWindowFocusChanged:(Z)V:GetOnWindowFocusChanged_ZHandler\n" +
+			"";
+		mono.android.Runtime.register ("FrameAnimation.Activity1, FrameAnimation, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", Activity1.class, __md_methods);
+	}
+
+
+	public Activity1 ()
+	{
+		super ();
+		if (getClass () == Activity1.class)
+			mono.android.TypeManager.Activate ("FrameAnimation.Activity1, FrameAnimation, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onWindowFocusChanged (boolean p0)
+	{
+		n_onWindowFocusChanged (p0);
+	}
+
+	private native void n_onWindowFocusChanged (boolean p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
